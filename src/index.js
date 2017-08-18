48bfc14e39bcf112c7faf469f9e0e6bdf5b26a45
TimPerry1994 <timperry@fsmail.net>
2017-08-18 13:32:06 +0100

    Create app template

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Item extends React.Component {
+	// TODO
+}
+
+class ShoppingBag extends React.Component {
+	// TODO
+}
+
+function addItem(name, price) {
+	// TODO
+}
+
+function removeItem(name) {
+	// TODO
+}
+
+function checkList() {
+	// TODO
+}
